65cc3cfde1d3259af1baf6d7731beee978d8e003
Saurabh Verma <saurabhshalu@gmail.com>
2020-03-28 23:19:52 +0530

    details bugs

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -12,6 +12,7 @@ export class DetailsComponent implements OnInit {
   countofdorm: string = '0';
   tweetcount: string = '0';  
   lineChartData: any = null;
+  public newChartData: any = null;
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
@@ -21,13 +22,16 @@ export class DetailsComponent implements OnInit {
     if (this.hashtag.length==0 || this.dorm == "-1" || this.countofdorm=="0" || this.tweetcount=="0") {
       return
     }
+    (<HTMLInputElement>document.getElementById("overlay")).style.display = "block";
     this.http.get('http://127.0.0.1:8000/reqres?hashtag=' + this.hashtag.replace('#','') + ' &type=1&dorm=' + this.dorm + '&countofdorm=' + this.countofdorm + '&tcount=' + this.tweetcount).subscribe((data: any[])=>{ 
-
-      this.lineChartData = data;
+     this.lineChartData = data; 
+     this.newChartData = [this.lineChartData["positive"].toFixed(1), this.lineChartData["negative"].toFixed(1), (100 - (this.lineChartData["positive"] + this.lineChartData["negative"])).toFixed(1)];
       console.log(data);
+      (<HTMLInputElement>document.getElementById("overlay")).style.display = "none";
     },
     (err: any[])=>{
       console.log(err);
+      (<HTMLInputElement>document.getElementById("overlay")).style.display = "none";
     })
   }
 }
