83fb5265163dd82986cf28f08f68bbc92eef1005
Saurabh Verma <saurabhshalu@gmail.com>
2020-05-07 03:49:57 +0530

    test5

--- src/app/Feedback/App.FeedbackComponent.ts
+++ src/app/Feedback/App.FeedbackComponent.ts
@@ -6,9 +6,37 @@ import { HttpParams, HttpClient } from '@angular/common/http';
     styleUrls: ['./App.FeedbackComponent.css']
 })
 export class FeedbackComponentNew {
+    public name: string;
+    public email: string;
+    public url: string;
+    public subject: string;
     constructor(private http: HttpClient) {}
+    ngOnInit() {
+        this.name = "";
+        this.email = "";
+        this.url = "";
+        this.subject = "";
+    }
+    // testSubmit() {
+    //     const body1 = new HttpParams().set('form-name','contact').append('name','saurabh').append('email','saurabh111@gmail.com').append('message','here is test message');
+
+    //     this.http.post('/', body1.toString(), {headers: { 'Content-Type': 'application/x-www-form-urlencoded' }, responseType: 'text'}).subscribe(
+    //       res => {
+    //           console.log(res);
+    //       },
+    //       err => {
+    //           console.log(err);
+    //       }
+    //     );
+    // }
+
+
     testSubmit() {
-        const body1 = new HttpParams().set('form-name','contact').append('name','saurabh').append('email','saurabh111@gmail.com').append('message','here is test message');
+        const body1 = new HttpParams().set('form-name','contact')
+        .append('name',this.name)
+        .append('email',this.email)       
+        .append('tweeturl',this.url)
+        .append('subject',this.subject);
 
         this.http.post('/', body1.toString(), {headers: { 'Content-Type': 'application/x-www-form-urlencoded' }, responseType: 'text'}).subscribe(
           res => {
