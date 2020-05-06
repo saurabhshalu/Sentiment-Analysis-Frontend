3b9cb84fc4909449d1785e809a300ad9f0223b52
Saurabh Verma <saurabhshalu@gmail.com>
2020-05-07 03:35:42 +0530

    test2

--- src/app/Feedback/App.FeedbackComponent.ts
+++ src/app/Feedback/App.FeedbackComponent.ts
@@ -8,9 +8,9 @@ import { HttpParams, HttpClient } from '@angular/common/http';
 export class FeedbackComponentNew {
     constructor(private http: HttpClient) {}
     testSubmit() {
-        const body1 = new HttpParams().set('form-name','contact').append('name','saurabh').append('email','saurabh@gmail.com').append('message','here is test message');
+        const body1 = new HttpParams().set('form-name','contact').append('name','saurabh').append('email','saurabh111@gmail.com').append('message','here is test message');
 
-        this.http.post('/', body1.toString(), {headers: { 'Content-Type': 'application/x-www-form-urlencoded' }}).subscribe(
+        this.http.post('/', body1.toString(), {headers: { 'Content-Type': 'application/x-www-form-urlencoded' }, responseType: 'text'}).subscribe(
           res => {
               alert(res);
           }
